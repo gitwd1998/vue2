f665957b3302dc4157810caee28f08c873023ad0
gitwd1998 <wd15855385074@outlook.com>
2024-03-16 13:46:01 +0800

    test: sentry

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -3,7 +3,7 @@ import router from '@/router'
 import * as Sentry from '@sentry/vue'
 
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "development") {
   Sentry.init({
     Vue,
     dsn: "https://9c59d2417b7d8e1075e830ddddadee6c@o4505568238632960.ingest.sentry.io/4505650819497984",
